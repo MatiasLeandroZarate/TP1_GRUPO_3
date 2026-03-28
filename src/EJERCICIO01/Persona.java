717fbb4f4268a1216c629f53678b9a2c540abe08
Alejandro-Madero <madero.ale@gmail.com>
2026-03-28 03:12:31 -0300

    Primer commit, clase persona

---
+++ src/EJERCICIO01/Persona.java
@@ -0,0 +1,15 @@
+package EJERCICIO01;
+import java.time.LocalDate;
+
+
+public class Persona {
+
+String dni;
+String nombre;
+String apellido;
+LocalDate fechaNacimiento;
+String genero;
+String direccion;
+String telefono;
+String email;
+}
