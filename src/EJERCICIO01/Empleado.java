a1c9e7f88f54c0f81a2ce37e074c6263b4b583d6
Matias Zarate <m.zarate92@hotmail.com>
2026-03-29 15:17:25 -0300

    Getters, Setters y toString

--- src/EJERCICIO01/Empleado.java
+++ src/EJERCICIO01/Empleado.java
@@ -2,9 +2,8 @@ package EJERCICIO01;
 
 import java.time.LocalDate;
 
-public class Empleado extends Persona{
-
-	
+public class Empleado extends Persona
+{
 	private int legajo;
 	private String puesto; 
 	
@@ -14,5 +13,23 @@ public class Empleado extends Persona{
 	public Empleado(String dni,String nombre, String apellido, LocalDate fechaNacimiento, String genero, String direccion, String telefono, String email, String puesto) {};
 	
 	//Getters y Setters
+	
+	public int getLegajo() {
+		return legajo;
+	}
+	public void setLegajo(int legajo) {
+		this.legajo = legajo;
+	}
+	public String getPuesto() {
+		return puesto;
+	}
+	public void setPuesto(String puesto) {
+		this.puesto = puesto;
+	}
+	@Override
+	public String toString() {
+		return "Empleado [Legajo=" + legajo +  ", Puesto=" + puesto +  "]" + ", " + super.toString();
+	}
+	
 	
 }
